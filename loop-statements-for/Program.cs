4d615a3ebfbbca9f9d13af7c4e4ade3c08627288
Iosif Turcu <iosif.turcu@emmanuel.ro>
2021-11-05 10:25:39 +0200

    returns the text normaly and reversed

---
+++ loop-statements-for/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace loop_statements_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a text: ");
+            string text = Console.ReadLine();
+
+            for(int i = 0; i < text.Length; i++)
+            {
+                Console.Write(text[i]);
+            }
+            Console.WriteLine();
+            for(int i = text.Length-1; i>= 0; i--)
+            {
+                Console.Write(text[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
